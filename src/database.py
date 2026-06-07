c96b925e3aad55e87738145974fbcee84a0cdf77
Diego <diegoo.pbe@gmail.com>
2026-06-07 17:54:50 -0300

    fix: correção da url de conexão com banco de dados

--- src/database.py
+++ src/database.py
@@ -1,7 +1,7 @@
 import os
 
 from dotenv import load_dotenv
-from sqlalchemy import create_engine
+from sqlalchemy import URL, create_engine
 from sqlalchemy.orm import sessionmaker
 
 load_dotenv()
@@ -12,11 +12,22 @@ DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAMESPACE = os.getenv("DB_NAMESPACE")
 
-DATABASE_URL = (
-    f"iris+intersystems://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAMESPACE}"
+# DATABASE_URL = (
+#     f"iris+intersystems://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAMESPACE}"
+# )
+if not (DB_USER and DB_PASSWORD and DB_HOST and DB_PORT and DB_NAMESPACE):
+    raise ValueError("configuração incompleta, checar arquivo de configuração ambientes.")
+
+DATABASE_URL = URL.create(
+    drivername="iris+intersystems",
+    username=DB_USER,
+    password=DB_PASSWORD,
+    host=DB_HOST,
+    port=int(DB_PORT),
+    database=DB_NAMESPACE,
 )
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True, connect_args={"ssl": False})
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
