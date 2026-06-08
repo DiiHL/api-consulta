62e42c0bd52fe64ce9f2298e28c7dae465667608
Diego <diegoo.pbe@gmail.com>
2026-06-08 00:15:11 -0300

    chore: atualizando documentação main

--- src/main.py
+++ src/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI
 
 from routers.atendimento_router import router as atendimento_router
 
-app = FastAPI(title="API de Atendimento", version="1.0.0")
+app = FastAPI(
+    title="API de Atendimento",
+    description="API para consulta de indicadores de atendimentos por unidade",
+    version="1.0.0",
+)
 
 app.include_router(atendimento_router)
